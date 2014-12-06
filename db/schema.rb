1afe5bc52ea98605bd2b8ac664c4eb038bfddc1c
Javier Cuevas <javi@diacode.com>
2014-12-06 20:46:19 +0100

    Set 0 as default score for users

--- db/schema.rb
+++ db/schema.rb
@@ -53,7 +53,7 @@ ActiveRecord::Schema.define(version: 20141206194344) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.integer  "score"
+    t.integer  "score",                  default: 0
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
