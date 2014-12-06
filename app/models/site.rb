975d685725b2d7c39bf65c497f5a4ba4119cd1b1
Javier Cuevas <javi@diacode.com>
2014-12-06 20:20:14 +0100

    Adding auditors working, yay! ;D

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,5 +1,7 @@
 class Site < ActiveRecord::Base
   belongs_to :user
   has_many :auditor_participations
+  has_many :auditors, through: :auditor_participations, source: :user
   has_many :tester_participations
+
 end
