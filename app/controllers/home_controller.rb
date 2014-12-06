eb98abf6a1496282e5b950f6ecfcc5ee6eb05d19
Pablo Lopez Torres <pablolopeztorres@gmail.com>
2014-12-06 17:50:41 +0100

    adding mvp template and base layout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,9 @@ class HomeController < ApplicationController
   def show
     user_id = session[:user_id]
     if user_id
-      user = User.find(user_id)
+      @user = User.find(user_id)
+    else
+      @user = nil
     end
-
-    render json: user
   end
 end
