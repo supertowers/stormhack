6ace8b0f3ee9502885733538014e4cbd2a0b1f37
Raúl Marcos <raulmarcosl@gmail.com>
2014-12-06 16:38:04 +0100

    Add user show's proof of concept

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController
   def show
+    user_id = session[:user_id]
+    if user_id
+      user = User.find(user_id)
+    end
+
+    render json: user
   end
 end
