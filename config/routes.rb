6729152b0dc5713dc0d2e1160ebe664247862e8e
Raúl Marcos <raulmarcosl@gmail.com>
2014-12-06 20:12:59 +0100

    Fix logout

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,6 @@ Rails.application.routes.draw do
 
   # facebook login
   get 'auth/:provider/callback', to: 'sessions#create'
-  get 'logout', to: 'sessions#destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
