dc228d2492a6a727c07c6992aad992d715b5dcc1
Pablo Lopez Torres <pablolopeztorres@gmail.com>
2014-12-06 15:43:27 +0100

    adding omniauth facebook

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  root 'home#show'
+
+  # facebook login
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
