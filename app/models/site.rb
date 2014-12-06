be64abf9d60b139d9308260eab69e5eea5995126
Javier Cuevas <javi@diacode.com>
2014-12-06 21:00:35 +0100

    Automatically add http to sites if missing

--- app/models/site.rb
+++ app/models/site.rb
@@ -4,4 +4,12 @@ class Site < ActiveRecord::Base
   has_many :auditors, through: :auditor_participations, source: :user
   has_many :tester_participations
 
+  before_validation :smart_add_url_protocol
+
+  private
+    def smart_add_url_protocol
+      unless self.url[/\Ahttp:\/\//] || self.url[/\Ahttps:\/\//]
+        self.url = "http://#{self.url}"
+      end
+    end
 end
