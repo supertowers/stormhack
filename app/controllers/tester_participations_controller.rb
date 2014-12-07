532d73edfd159025928d252f1e69c86564597532
Raúl Marcos <raulmarcosl@gmail.com>
2014-12-07 03:07:00 +0100

    Add tester controller

---
+++ app/controllers/tester_participations_controller.rb
@@ -0,0 +1,42 @@
+class TesterParticipationsController < ApplicationController
+  before_action :set_site
+
+  # POST /sites/1/tester_participations
+  # POST /sites/1/tester_participations.json
+  def create
+    @tester_participation = @site.tester_participations.build
+    @tester_participation.user = current_user
+
+    respond_to do |format|
+      if @tester_participation.save
+        format.html { redirect_to @site, notice: 'TesterParticipation was successfully created.' }
+        format.json { render :show, status: :created, location: @tester_participation }
+      else
+        format.html { redirect_to @site }
+        format.json { render json: @tester_participation.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /sites/1/tester_participations/1
+  # DELETE /sites/1/tester_participations/1.json
+  def destroy
+    @tester_participation = @site.tester_participations.find(params[:id])
+    @tester_participation.destroy
+
+    respond_to do |format|
+      format.html { redirect_to @site, notice: 'TesterParticipation was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def set_site
+      @site = Site.find(params[:site_id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def tester_participation_params
+      params.require(:participation).permit(nil)
+    end
+end
